597e99caf9e0060bae5bfbd421b1e762e6543a43
wicak29 <adi.wicaksana13@mhs.if.its.ac.id>
2017-04-19 03:47:52 +0700

    tambah csv yang ada tanggal

--- file/db 36.129/insert_daerah.sql
+++ file/db 36.129/insert_daerah.sql
@@ -1,4 +1,4 @@
-INSERT INTO `daerah` (`id_daerah`, `kode_daerah`, `nama_daerah`, `date_change`) VALUES
+INSERT INTO `daerah` (`iddaerah`, `kode`, `nama`, `date_change`) VALUES
 	(1, 'SBY', 'Surabaya', '2017-04-08 16:47:11'),
 	(2, 'JKT', 'Jakarta', '2017-04-08 16:47:09'),
 	(3, 'BDG', 'Bandung', '2017-04-08 16:47:10'),
@@ -8,3 +8,11 @@ INSERT INTO `daerah` (`id_daerah`, `kode_daerah`, `nama_daerah`, `date_change`) 
 	(7, 'CGK', 'Cengkareng', '2017-04-09 14:57:30'),
 	(8, 'BUD', 'Ubud', '2017-04-09 14:57:30'),
 	(9, 'SGJ', 'Singaraja', '2017-04-09 14:57:30');
+
+INSERT INTO daerah3 VALUES (10, 'MED', 'MEDAN', '2017-04-15 15:24:14');
+INSERT INTO daerah3 VALUES (11, 'PAD', 'Padang', '2017-04-15 15:24:15');
+
+DELETE FROM daerah3 WHERE iddaerah = 1;
+DELETE FROM daerah3 WHERE iddaerah = 3;
+
+
